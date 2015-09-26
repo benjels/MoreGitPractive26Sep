072f5bfcd7846f41262cb1720e3e8e91a8f8f0d7
maxBrown <maxbrown0101@gmail.com>
2015-09-26 20:53:40 +1200

    added another class yah

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 
 	public static void main(String[] args){
-		System.out.println("eat shit");
-		System.out.println("git is based as fuck tbh");
+		Printer.printText("we love git tbh");
 	}
 }
---
+++ src/Printer.java
@@ -0,0 +1,12 @@
+/**
+ * prints passed text to default lol
+ * @author brownmax1
+ *
+ */
+public class Printer {
+
+
+	public static void printText(String someText){
+		System.out.println(someText);
+	}
+}
